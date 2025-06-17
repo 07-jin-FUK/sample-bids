ec8c22e6566a4f19dfba17a1a49b40e3e5231135
Jin Yamamoto <jin@JinnoMacBook-Pro.local>
2025-06-17 12:39:53 +0900

    [update]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/style.scss'],
+  css: ['~/assets/scss/style.scss'],
   app: {
     head: {
       script: [
